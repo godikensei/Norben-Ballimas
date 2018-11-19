340e60d3ccb15e55e602f52c3d3a354ed743c589
godikensei <godikensei@gmail.com>
2018-11-19 18:05:50 +0100

    Rewind

---
+++ Ballimas/Norben-Ballimas/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    float horizontalMove = 0.0f;
+    public float speed;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+}
