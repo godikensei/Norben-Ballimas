51453c4fda1bb931e9608b3033eb27efe39f1eb3
godikensei <godikensei@gmail.com>
2018-12-12 20:39:13 +0100

    2

---
+++ Ballimas/Norben-Ballimas/Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+
+    public float offsetX;
+
+    Material material;
+    Vector2 offset;
+
+
+    // Use this for initialization
+    void Awake () {
+        material = GetComponent<Renderer>().material;
+	}
+
+    private void Start()
+    {
+        offset = new Vector2(offsetX, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        material.mainTextureOffset += offset * Time.deltaTime;
+	}
+}
