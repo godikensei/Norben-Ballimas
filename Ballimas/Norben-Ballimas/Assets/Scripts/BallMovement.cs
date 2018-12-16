d9ecd077022153ded4d4aaa8a4fc933e17b7899b
godikensei <godikensei@gmail.com>
2018-12-16 15:17:55 +0100

    New movement

--- Ballimas/Norben-Ballimas/Assets/Scripts/BallMovement.cs
+++ Ballimas/Norben-Ballimas/Assets/Scripts/BallMovement.cs
@@ -6,8 +6,11 @@ public class BallMovement : MonoBehaviour {
 
     float horizontalMove = 0.0f;
     public float speed;
+    public float bounceBack;
 
     private Rigidbody2D rb;
+
+    public bool onGround = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,8 +20,24 @@ public class BallMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         horizontalMove = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontalMove * speed, Mathf.Clamp(rb.velocity.y, -speed, speed));
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Floor")
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Block")
+        {
+            rb.velocity = new Vector2(0.0f, rb.velocity.y + bounceBack);
+        }
+    }
 
     void FixedUpdate()
     {
